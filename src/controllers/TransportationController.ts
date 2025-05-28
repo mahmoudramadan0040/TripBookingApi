4d13d833c8fa1796b5bd343b2bf7c255fb2a7685
mahmoudramadan0040 <mahmoudramadan0040@gmail.com>
2025-05-28 06:41:29 +0200

    still working

--- src/controllers/TransportationController.ts
+++ src/controllers/TransportationController.ts
@@ -1,96 +1,128 @@
-import { Request , Response , NextFunction } from "express";
-import transportation from "../models/transportation.model";
-import { console } from "inspector";
-class TransportationController{ 
+import { Request, Response, NextFunction } from 'express'
+import transportation from '../models/transportation.model'
+class TransportationController {
+  // Create Transportation
+  createTransportation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {
+        title,
+        Duration,
+        Description,
+        included,
+        excluded,
+        highlight,
+        price,
+      } = req.body
 
-// Create Transportation
-createTransportation = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+      // Get Cloudinary uploaded files (if using fieldname: cloudImages)
+      const cloudFiles = (req.files as any)?.cloudImages || []
+      const localFiles = (req.files as any)?.localImages || []
+      const cloudUrls = Array.isArray(cloudFiles)
+        ? cloudFiles.map((file: any) => file.path)
+        : [] // Cloudinary URL
+      const localPaths = Array.isArray(localFiles)
+        ? localFiles.map((file: Express.Multer.File) => `/${file.filename}`)
+        : []
 
-    const { title, Duration, Description, included, excluded, highlight,price } = req.body;
+      console.log("=====================included=================")
+      console.log([included])
+      console.log(typeof(included))
 
-    // Get Cloudinary uploaded files (if using fieldname: cloudImages)
-    const cloudFiles = (req.files as any)?.cloudImages || [];
-    const localFiles = (req.files as any)?.localImages || [];
-    const cloudUrls =Array.isArray(cloudFiles) ? cloudFiles.map((file: any) => file.path):[]; // Cloudinary URL
-    const localPaths =Array.isArray(localFiles) ? localFiles.map((file: Express.Multer.File) =>
-      `/${file.filename}`
-    ):[];
-    const newTransportation =await transportation.create({
-      title:title,
-      Duration:Duration,
-      Description:Description,
-      included:included as string[],
-      // excluded:excluded ,
-      // highlight:highlight ,
-      // price,
-      //  // Expecting array of image URLs or paths
-      // cloudImages:cloudUrls ,
-      // localImages:localPaths 
-    });
+      const newTransportation = await transportation.create({
+        title: title,
+        Duration: Duration,
+        Description: Description,
+        included: included as string[],
+        // excluded:excluded ,
+        // highlight:highlight ,
+        // price,
+        //  // Expecting array of image URLs or paths
+        // cloudImages:cloudUrls ,
+        // localImages:localPaths
+      })
 
-    res.status(201).json({ message: 'Transportation created', data: newTransportation });
-  } catch (error) {
-    next(error);
+      res
+        .status(201)
+        .json({ message: 'Transportation created', data: newTransportation })
+    } catch (error) {
+      next(error)
+    }
   }
-};
 
-// Get All
-getAllTransportation = async (req: Request, res: Response,next:NextFunction) => {
-  try {
-    const transportations = await transportation.findAll();
-    res.json({ data: transportations });
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching transportation', error });
+  // Get All
+  getAllTransportation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const transportations = await transportation.findAll()
+      res.json({ data: transportations })
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching transportation', error })
+    }
   }
-};
 
-// Get by ID
-getTransportationById = async (req: Request, res: Response,next:NextFunction) => {
-  try {
-    const { id } = req.params;
-    const result = await transportation.findByPk(id);
-    if (!result)  res.status(404).json({ message: 'Not found' });
+  // Get by ID
+  getTransportationById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const { id } = req.params
+      const result = await transportation.findByPk(id)
+      if (!result) res.status(404).json({ message: 'Not found' })
 
-    res.json({ data: transportation });
-  } catch (error) {
-    // res.status(500).json({ message: 'Error fetching transportation', error });
-    next(error)
+      res.json({ data: transportation })
+    } catch (error) {
+      // res.status(500).json({ message: 'Error fetching transportation', error });
+      next(error)
+    }
   }
-};
 
-// Update
-updateTransportation = async (req: Request, res: Response,next:NextFunction) => {
-  try {
-    const { id } = req.params;
-    const [updated] = await transportation.update(req.body, { where: { id } });
+  // Update
+  updateTransportation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const { id } = req.params
+      const [updated] = await transportation.update(req.body, { where: { id } })
 
-    if (!updated)  res.status(404).json({ message: 'Not found or no changes' });
+      if (!updated) res.status(404).json({ message: 'Not found or no changes' })
 
-    const updatedTransportation = await transportation.findByPk(id);
-    res.json({ message: 'Updated successfully', data: updatedTransportation });
-  } catch (error) {
-    // res.status(500).json({ message: 'Error updating transportation', error });
-    next(error)
+      const updatedTransportation = await transportation.findByPk(id)
+      res.json({ message: 'Updated successfully', data: updatedTransportation })
+    } catch (error) {
+      // res.status(500).json({ message: 'Error updating transportation', error });
+      next(error)
+    }
   }
-};
 
-// Delete
-deleteTransportation = async (req: Request, res: Response,next:NextFunction) => {
-  try {
-    const { id } = req.params;
-    const deleted = await transportation.destroy({ where: { id } });
+  // Delete
+  deleteTransportation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const { id } = req.params
+      const deleted = await transportation.destroy({ where: { id } })
 
-    if (!deleted)  res.status(404).json({ message: 'Not found' });
+      if (!deleted) res.status(404).json({ message: 'Not found' })
 
-    res.json({ message: 'Deleted successfully' });
-  } catch (error) {
-    // res.status(500).json({ message: 'Error deleting transportation', error });
-    next(error)
+      res.json({ message: 'Deleted successfully' })
+    } catch (error) {
+      // res.status(500).json({ message: 'Error deleting transportation', error });
+      next(error)
+    }
   }
-};
-
-
 }
 
-export default TransportationController;
+export default TransportationController
