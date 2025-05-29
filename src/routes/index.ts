8257a9c264ad04943d776084e03538e39df34a5a
mahmoudramadan0040 <mahmoudramadan0040@gmail.com>
2025-05-29 13:36:07 +0200

    add vercel file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import authRoute from "./api/authRoute";
 import transportRoute from "./api/trasnportRoute"
+import tourRoute from "./api/tourRoute"
 const routes =Router();
 
+routes.use("/tour",tourRoute);
 routes.use("/transport",transportRoute);
 routes.use("/auth",authRoute);
 export default routes;
