05d750a2edd3833c216da664d9481e216adcac0c
mahmoudramadan0040 <mahmoudramadan0040@gmail.com>
2025-05-29 14:35:39 +0200

    cancel deploy in vercel

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// api/index.ts
-import { IncomingMessage, ServerResponse } from 'http'
-import app from '../src/index'
-
-export default (req: IncomingMessage, res: ServerResponse) => {
-  app(req, res)
-}
