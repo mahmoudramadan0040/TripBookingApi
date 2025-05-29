e8cedf433513b95f3baa01dceeaa8ee0105861bc
mahmoudramadan0040 <mahmoudramadan0040@gmail.com>
2025-05-29 14:09:10 +0200

    still try deploy

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,7 @@
-import app from "../src";
-export default app;
+// api/index.ts
+import { IncomingMessage, ServerResponse } from 'http'
+import app from '../src/index'
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  app(req, res)
+}
